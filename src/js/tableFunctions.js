7f740dc4c3368fe2cbaca0b961831bd431dcb06d
Felipe Catão do Nascimento <cataofelipe@gmail.com>
2020-06-27 16:49:20 -0300

    CreateCol Finish

--- src/js/tableFunctions.js
+++ src/js/tableFunctions.js
@@ -1,15 +1,16 @@
 var element=document.getElementById("addColumn01")
 var fristUse=true
+var idCol=0
 function AddNewColumn(){
     let elementTodreate=document.createElement('div')
     let elementAppend = document.getElementById("bodyModal")
     if(fristUse==true){
-        
+
         element.style.visibility="hidden"
         elementTodreate.classList+="addnewColumn"
-        elementTodreate.id="addColumn3"
+        elementTodreate.id="addColumn"+idCol
         elementAppend.appendChild(elementTodreate)
-        elementAppend=document.getElementById("addColumn3")
+        elementAppend=document.getElementById("addColumn"+idCol)
         elementTodreate=document.createElement('label')
         elementAppend.appendChild(elementTodreate)        
         elementTodreate.innerHTML+="Nome da Coluna: "
@@ -17,14 +18,33 @@ function AddNewColumn(){
         elementAppend.appendChild(elementTodreate)
         elementTodreate=document.createElement('button')
         elementTodreate.classList+="btAddTable"
-        elementTodreate.id="btAddColumn01"
+        elementTodreate.id="btAddColumn"+idCol
         elementAppend.appendChild(elementTodreate)
         element.removeEventListener("click",AddNewColumn,false)
         elementTodreate.addEventListener("click",AddNewColumn,false)
         fristUse=false
+        idCol+=1
     }
     else{
-        console.log("só copiar o codigo de cima")
+        element=document.getElementById("btAddColumn"+(idCol-1))
+        element.style.visibility="hidden"
+        elementTodreate.classList+="addnewColumn"
+        elementTodreate.id="addColumn"+idCol
+        elementAppend.appendChild(elementTodreate)
+        elementAppend=document.getElementById("addColumn"+idCol)
+        elementTodreate=document.createElement('label')
+        elementAppend.appendChild(elementTodreate)        
+        elementTodreate.innerHTML+="Nome da Coluna: "
+        elementTodreate=document.createElement('input')
+        elementAppend.appendChild(elementTodreate)
+        elementTodreate=document.createElement('button')
+        elementTodreate.classList+="btAddTable"
+        elementTodreate.id="btAddColumn"+idCol
+        elementAppend.appendChild(elementTodreate)
+        element.removeEventListener("click",AddNewColumn,false)
+        elementTodreate.addEventListener("click",AddNewColumn,false)
+        fristUse=false
+        idCol+=1
     }
 }
 
