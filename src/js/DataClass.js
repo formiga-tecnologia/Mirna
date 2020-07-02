55a7fff0b2602c04cd4cbf3fca474df47ee5f21a
Felipe Catão do Nascimento <cataofelipe@gmail.com>
2020-07-01 21:52:24 -0300

    Open data Base set

---
+++ src/js/DataClass.js
@@ -0,0 +1,10 @@
+class DataClass{
+    constructor(NameData,Columns,Inputs){
+        nameTable=NameData
+        ColumnsData = Columns
+        InputsValue = Inputs
+    }
+    showData(){
+        console.log(this.nameTable)
+    }
+}
